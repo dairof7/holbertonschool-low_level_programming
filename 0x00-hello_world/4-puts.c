2d0031dd8f187b92c2368c37054c984af0dbeded
root <1564@holbertonschool.com>
2020-02-05 19:49:22 +0000

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  * Print a text
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  char s[]="\"Programming is like building a multilingual puzzle";
-  puts(s);
-  return(0);
+	char s[] = "\"Programming is like building a multilingual puzzle";
+		puts(s);
+		return (0);
 }
