565ffacdf318282e470cebf527a15e8cd2b6a795
root <1564@holbertonschool.com>
2020-02-05 23:17:24 +0000

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,18 @@
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+ *main - prints line
+ *Return: 1
+*/
+
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	int l;
+
+	l = sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
+
+	write(2,
+	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n", l);
 	return (1);
 }
