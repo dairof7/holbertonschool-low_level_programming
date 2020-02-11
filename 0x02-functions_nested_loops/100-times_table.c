48851b429631e27c214e1717fcb561029b944f7e
dairof7 <dairof7@gmail.com>
2020-02-11 13:29:22 -0500

    fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,12 +14,14 @@ void print_times_table(int n)
 		for (k = 0; k <= n; k++)
 		{
 			res = j * k;
-			if (res < 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(res % 10 + '0');
-			}
+			if (k == 0)
+				_putchar('0');
+			else if (((res < 10) && (res > 0)) || (j == 0))
+                        {
+                                _putchar(' ');
+                                _putchar(' ');
+                                _putchar(res % 10 + '0');
+                        }
 			else if (res < 100)
 			{
 				_putchar(' ');
