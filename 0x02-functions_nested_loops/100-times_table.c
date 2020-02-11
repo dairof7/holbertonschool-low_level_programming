90f11cb5438bc06879cd427a34d532c89cb3fead
dairof7 <dairof7@gmail.com>
2020-02-11 12:59:16 -0500

    fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,48 +8,38 @@ void print_times_table(int n)
 	int j, k, res;
 
 	if ((n <= 15) && (n > 0))
+	break;
+	for (j = 0; j <= n; j++)
 	{
-		for (j = 0; j <= n; j++)
+		for (k = 0; k <= n; k++)
 		{
-			for (k = 0; k <= n; k++)
+			res = j * k;
+			if (res < 10)
 			{
-				res = j * k;
-				if (k == 0)
-				{
-					_putchar('0');
-				}
-				else
-				{
-					if  ((res / 100) == 0)
-					{
-						_putchar(' ');
-						if (((res / 10)) % 10 == 0)
-						{
-							_putchar(' ');
-							_putchar((res % 10) + '0');
-						}
-						else
-						{
-							_putchar(((res / 10) % 10) + '0');
-							_putchar((res % 10) + '0');
-
-						}
-
-					}
-					else
-					{
-						_putchar((res / 100) + '0');
-						_putchar(((res / 10) % 10) + '0');
-						_putchar((res % 10) + '0');
-					}
-				}
-				if (k != n)
-				{
-				_putchar(',');
 				_putchar(' ');
-				}
+				_putchar(' ');
+				_putchar(res % 10 + '0');
 			}
-			_putchar('\n');
+			else if (res < 100)
+			{
+				_putchar(' ');
+				_putchar(((res / 10) % 10) + '0');
+				_putchar((res % 10) + '0';
+			}
+			else
+			{
+				_putchar((res / 100) + '0');
+				_putchar(((res / 10) % 10) + '0');
+				_putchar((res % 10) + '0');
+			}
+
+			if (k != n)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
 		}
+		_putchar('\n');
 	}
+
 }
