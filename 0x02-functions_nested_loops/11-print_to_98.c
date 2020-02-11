e146bb64a34225b0431ff970ee95085fabf52ef6
dairof7 <dairof7@gmail.com>
2020-02-11 09:55:07 -0500

    fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,9 +7,10 @@
 
 void print_to_98(int n)
 {
+	int i;
 	if (n <= 98)
 	{
-		for (int i = n; i <= 98; i++)
+		for (i = n; i <= 98; i++)
 		{
 			if (i == 98)
 				printf("%d\n", i);
@@ -19,7 +20,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (int i = n; i >= 98; i--)
+		for (i = n; i >= 98; i--)
 		{
 			if (i == 98)
 				printf("%d\n", i);
