3b849c067d196af188298d8a3e6c0a66a17c9d81
dairof7 <dairof7@gmail.com>
2020-02-11 21:02:05 -0500

    add file

---
+++ 0x03-debugging/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, count;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0)
+			count = count + i
+	}
+	printf("%d\n", count);
+	return (0);
+}
+
+
