c912c6aaa914feb8a1e9a86fc4a8bb54b2c7b378
dairof7 <dairof7@gmail.com>
2020-02-11 21:05:37 -0500

    fix file

--- 0x03-debugging/101-natural.c
+++ 0x03-debugging/101-natural.c
@@ -10,11 +10,9 @@ int main(void)
 
 	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 3 == 0) || (i % 5 == 0)
-			count = count + i
+		if ((i % 3 == 0) || (i % 5 == 0))
+			count = count + i;
 	}
 	printf("%d\n", count);
 	return (0);
 }
-
-
