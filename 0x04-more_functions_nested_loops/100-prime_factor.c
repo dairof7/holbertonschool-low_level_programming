c3e9d9545af3ae89e5c173dff1fdbd1c4c9479fd
dairof7 <dairof7@gmail.com>
2020-02-13 16:45:21 -0500

    add

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int primo = 2, res = 0, d, ne = 612852475143;
+
+		while (ne >= primo)
+		{
+			for (d = 2; d < primo; ++d)
+			{
+				if (primo % d == 0)
+					break;
+			}
+			if (d == primo)
+				while (ne % primo == 0)
+				{
+					if (ne != primo)
+					{
+						printf("%ld, ", primo);
+						res = ne / primo;
+						ne = res;
+					}
+			else
+			{
+				printf("%ld\n", primo);
+				break;
+			}
+		}
+		primo++;
+	}
+}
