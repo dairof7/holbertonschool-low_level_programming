4c804ffb4a09fdade6b3aeec12b46319676d46e2
dairof7 <dairof7@gmail.com>
2020-02-20 15:55:58 -0500

    fix

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -13,7 +13,7 @@ char *string_toupper(char *t)
 int i;
 	for (i = 0; t[i] != '\0'; i++)
 	{
-		if ((t[i] > 'a') && (t[i] < 'z'))
+		if ((t[i] >= 'a') && (t[i] <= 'z'))
 			t[i] = t[i] - 32;
 	}
 	return (t);
