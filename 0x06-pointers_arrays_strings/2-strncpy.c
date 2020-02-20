5c4f930161d248861624acd50cd999bbbdd9fd07
dairof7 <dairof7@gmail.com>
2020-02-20 13:15:11 -0500

    fix

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,7 +13,10 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (j = 0; j < n && src[j] != '\0'; j++)
 		dest[j] = src[j];
-	dest[j] = '\0';
-
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
 	return (dest);
 }
