a13d74d469d501a6886158856aae77d5db70c67f
dairof7 <dairof7@gmail.com>
2020-02-20 11:09:45 -0500

    add files

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * *_strncat - concatenate strings
+ * @dest: array
+ * @src: array
+ * @n: numers os bytes to concatenate
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; j < n && src[j] != '\0'; j++)
+		dest[i + j] = src[j];
+	dest[i + j] = '\0';
+
+	return (dest);
+}
