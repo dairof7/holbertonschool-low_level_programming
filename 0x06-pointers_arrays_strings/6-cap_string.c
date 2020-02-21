500a700f488d23c06595af8400473e57d1cb66ff
dairof7 <dairof7@gmail.com>
2020-02-20 19:33:00 -0500

    fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,14 +10,21 @@
 
 char *cap_string(char *t)
 {
-int i;
+	int i, sw = 1, j;
+	char separator[] = {" \t\n,;.!?\"(){}"};
+
 	for (i = 0; t[i] != '\0'; i++)
 	{
-		if (t[i] == '\t')
-			t[i] = ' ';
-		if ((t[i] >= 'a') && (t[i] <= 'z') && ((t[i - 1] == ' ') ||
-		(t[i - 1] == '\n') || (t[i - 1] == '.')))
+		if ((t[i] >= 'a') && (t[i] <= 'z') && (sw == 1))
 			t[i] = t[i] - 32;
+		sw = 0;
+
+		for (j = 0; j <= 12; j++)
+		{
+			if (t[i] == separator[j])
+				sw = 1;
+		}
 	}
+
 	return (t);
 }
