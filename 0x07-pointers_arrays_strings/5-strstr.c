f95f2bf5923d082a7e2e0e8241096ec3ee9befbd
dairof7 <dairof7@gmail.com>
2020-02-24 21:19:40 -0500

    add file

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,41 +1,36 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * *_strpbrk - seachs string
- * @s: array in
- * @accept: char to search
- * Return: Always 0.
+ * *_strstr - search
+ * @needle: array in
+ * @haystack: arrayin
+ * Return: pointer or zero.
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int i, j=0, c=0, s=0;
+	unsigned int i, j = 0, c = 0, s = 0;
 
-while (needle[s] != '\0')
+	while (needle[s] != '\0')
 	{
 		s++;
 	}
-	
-printf("<%d>",s);		
-	
-for (i = 0; haystack[i] != '\0'; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		c=0;
+		c = 0;
 		for (j = 0; j < s; j++)
 		{
-			if 	((haystack[i+j] == needle [j]))
+			if (haystack[i + j] == needle[j])
 				{
-				c++;	
-				printf("<%d><%d>",c,i);	
+				c++;
 			}
 			else
 				break;
-		}			
-		if (c==s)
-		{
-		printf("<%d><%d>",c,s);
-			return (haystack + i);
+			}
+			if (c == s)
+			}
+				return (haystack + i);
+			}
 		}
-	}
-	return (haystack);
+	return (NULL);
 }
