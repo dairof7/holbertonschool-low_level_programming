5a6c4b9eb0b38bfb93acab754e20a687ae3b940f
dairof7 <dairof7@gmail.com>
2020-03-03 16:11:06 -0500

    add file

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int _strlen(char *s);
+/**
+ * *str_concat - create array
+ * @s1: input array
+ * @s2: input array
+ * Return: null in error
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, sizes1, sizes2, j = 0;
+	char *str_;
+
+	if ((s1 == NULL) && (s2 == NULL))
+		return (NULL);
+	sizes1 = _strlen(s1);
+	sizes2 = _strlen(s2);
+	str_ = malloc((sizeof(char) * sizes1 + sizeof(char) * sizes2) + 1);
+	if (sizes1 != 0 || sizes2 != 0)
+	{
+		while (i < sizes1)
+		{
+			*(str_ + i) = *(s1 + i);
+			i++;
+		}
+		while (j < sizes2)
+		{
+			*(str_ + i) = *(s2 + j);
+			j++;
+			i++;
+		}
+		*(str_ + i) = '\0';
+	}
+	else
+		return (NULL);
+	return (str_);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: char array pointer
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
