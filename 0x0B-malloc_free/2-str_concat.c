97ba9504812d4256dc3c59119801a7c2c71d2438
dairof7 <dairof7@gmail.com>
2020-03-03 16:28:54 -0500

    fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int _strlen(char *s);
 /**
  * *str_concat - create array
  * @s1: input array
@@ -10,13 +9,21 @@ int _strlen(char *s);
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, sizes1, sizes2, j = 0;
+	int i = 0, sizes1 = 0, sizes2 = 0, j = 0;
 	char *str_;
 
 	if ((s1 == NULL) && (s2 == NULL))
 		return (NULL);
-	sizes1 = _strlen(s1);
-	sizes2 = _strlen(s2);
+	else if (s1 == NULL)
+		*s1 = '\0';
+	else if (s1 == NULL)
+		*s2 = '\0';
+	while (*(s1 + sizes1))
+		sizes1++;
+	while (*(s2 + sizes2))
+		sizes2++;
+	sizes2++;
+
 	str_ = malloc((sizeof(char) * sizes1 + sizeof(char) * sizes2) + 1);
 	if (sizes1 != 0 || sizes2 != 0)
 	{
@@ -25,7 +32,7 @@ char *str_concat(char *s1, char *s2)
 			*(str_ + i) = *(s1 + i);
 			i++;
 		}
-		while (j < sizes2+1)
+		while (j < sizes2)
 		{
 			*(str_ + i) = *(s2 + j);
 			j++;
@@ -35,23 +42,4 @@ char *str_concat(char *s1, char *s2)
 	else
 		return (NULL);
 	return (str_);
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: char array pointer
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-
-	return (i);
 }
