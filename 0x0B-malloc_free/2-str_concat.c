c61fdeb2fcff0fab8636b24a87ec6114cab02096
dairof7 <dairof7@gmail.com>
2020-03-03 17:07:05 -0500

    fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,12 +15,13 @@ char *str_concat(char *s1, char *s2)
 
 	if ((s1 == NULL) && (s2 == NULL))
 		return (NULL);
-	else if (s1 == NULL)
+	
+	if (s1 == NULL)
 	{
 		s1 = malloc(1);
 		*s1 = '\0';
 	}
-	else if (s2 == NULL)
+	if (s2 == NULL)
 	{
 		s2 = malloc(1);
 		*s2 = '\0';
