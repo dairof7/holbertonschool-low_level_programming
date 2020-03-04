c249f57a24f68055da5c819e6f23ebc54a589933
dairof7 <dairof7@gmail.com>
2020-03-03 22:03:09 -0500

    add files and fix holberton.h

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * **strtow - concatenates all the arguments of your program
+ * @str: string
+ * Return: void
+ */
+
+char **strtow(char *str)
+{
+
+}
