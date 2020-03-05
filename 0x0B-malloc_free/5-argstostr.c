65d13e08620ad8a60aa9efa8e8bbe909907b3baa
dairof7 <dairof7@gmail.com>
2020-03-05 09:57:16 -0500

    fix

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -10,5 +10,33 @@
  */
 char *argstostr(int ac, char **av)
 {
+	int i = 0, j = 0, k = 0, size = 0;
+	char *str_;
 
+	if ((ac == 0) || (av == NULL))
+	{
+		return (0);
+	}
+	for (i = 0; av[i]; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			size++;
+	}
+	str_ = malloc(size * sizeof(char) + ac + 1);
+	if (str_ == NULL)
+	{
+		free(str_);
+		return (0);
+	}
+	for (i = 0; av[i]; i++)
+	{
+		for (j = 0; av[i][j]; j++, k++)
+		{
+			str_[k] = av[i][j];
+		}
+			str_[k] = '\n';
+		k++;
+	}
+	str_[k] = '\0';
+	return (str_);
 }
