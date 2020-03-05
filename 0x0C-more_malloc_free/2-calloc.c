e0df4cdd1c93439d77be2dfb9c6772ed03d2d715
dairof7 <dairof7@gmail.com>
2020-03-05 15:29:23 -0500

    add file

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * *_calloc - calloc function.
+ * @nmemb: number of elements.
+ * @size: size of variable.
+ * Return:  An array of two dimensional.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int j = 0;
+
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	for (j = 0; j < nmemb; j++)
+	{
+		p[j] = 0;
+	}
+	return ((void *)p);
+}
