617e3bd7507a3312c60562fdc96685f19740a102
dairof7 <dairof7@gmail.com>
2020-03-05 15:33:31 -0500

    fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,6 +13,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *p;
 	unsigned int j = 0;
 
+	if ((nmemb == 0) || (size == 0))
+		return (NULL);
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
