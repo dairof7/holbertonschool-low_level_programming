292b720e0a9a8c1530f0357a9f522c492f604cf0
dairof7 <dairof7@gmail.com>
2020-03-09 16:55:54 -0500

    fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,15 @@
 #ifndef DOG_
 #define DOG_
+
+/**
+ * struct dog - a dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: struct about the dog
+ */
+
 struct dog
 {
 	char *name;
