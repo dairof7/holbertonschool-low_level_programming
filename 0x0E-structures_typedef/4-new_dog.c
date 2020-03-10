a2f6e78cb8a72a65508513d75e207f7b983c937c
dairof7 <dairof7@gmail.com>
2020-03-09 19:46:20 -0500

    fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -45,8 +45,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		newname[k] = name[k];
 	for (k = 0; k <= j; k++)
 		newowner[k] = owner[k];
-	new->name = name;
+	new->name = newname;
 	new->age = age;
-	new->owner = owner;
+	new->owner = newowner;
 	return (new);
 }
