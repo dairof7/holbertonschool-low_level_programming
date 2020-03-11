437f84cdb14ec56b952a13df5e4962d2391cd960
dairof7 <dairof7@gmail.com>
2020-03-11 13:50:05 -0500

    fix betty

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * array_iterator -  Write a function that prints a name.
+ * @array: array of integers
+ * @size: size of array
+ * @action: function pointer
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
