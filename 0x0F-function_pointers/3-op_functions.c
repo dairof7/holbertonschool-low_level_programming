6a6ca47cb60df11c25fc26c7b1127c071b203b50
dairof7 <dairof7@gmail.com>
2020-03-11 18:56:48 -0500

    add files #3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,50 @@
+/**
+ * op_add -  add a to b.
+ * @a: int number
+ * @b: int number
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub -  subtraction between a and b.
+ * @a: int number
+ * @b: int number
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul -  multiplication between a and b.
+ * @a: int number
+ * @b: int number
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div -  division between a and b.
+ * @a: int number
+ * @b: int number
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod -  module between a and b.
+ * @a: int number
+ * @b: int number
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
