985d82cf77fc857e39ed1fbddc8be3052304eeae
dairof7 <dairof7@gmail.com>
2020-03-11 15:20:18 -0500

    fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,14 +11,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if ((array == NULL) || (cmp == NULL) || (size <= 0))
-		i = -1;
-	else
-	{
+		return (-1);
+
 		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]) == 1)
 				break;
 		}
-	}
-	return (i);
+		return (i);
 }
