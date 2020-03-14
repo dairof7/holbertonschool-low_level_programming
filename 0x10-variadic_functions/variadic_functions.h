67dcda2764f8dbf02ceb70e169e5b9eeb6670252
dairof7 <dairof7@gmail.com>
2020-03-14 12:44:15 -0500

    fix header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,16 @@
 #ifndef VARIADIC_FUNTIONS
 #define VARIADIC_FUNTIONS
-#include <stdarg.h> 
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+/**
+ * struct op - data struct
+ * @op: char option
+ * @f: function pointer
+ * Description: Longer description
+ */
 typedef struct op
 {
 	char *op;
