9b6b6172a546ef50906902e8a04ca2d387fb9431
dairof7 <dairof7@gmail.com>
2020-03-24 17:09:17 -0500

    fix 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -16,7 +16,11 @@ size_t print_listint_safe(const listint_t *head)
 		if (head > head->next)
 			head = head->next;
 		else
+		{
+			head = head->next;
+			printf("[%p]%d\n", (void *)head, head->n);
 			exit(98);
+		}
 	}
 
 	return (len);
