b6f4990bcbe214ea24de81e7f7d7972fbe58a007
dairof7 <dairof7@gmail.com>
2020-03-24 17:12:59 -0500

    fix error 98

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -18,7 +18,7 @@ size_t print_listint_safe(const listint_t *head)
 		else
 		{
 			head = head->next;
-			printf("[%p]%d\n", (void *)head, head->n);
+			printf("-> [%p]%d\n", (void *)head, head->n);
 			exit(98);
 		}
 	}
