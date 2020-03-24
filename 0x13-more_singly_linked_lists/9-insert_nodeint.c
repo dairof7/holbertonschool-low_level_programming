df46c952371cf446f46dbca500463fbd7d5d9e61
dairof7 <dairof7@gmail.com>
2020-03-24 10:53:57 -0500

    fix #9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,6 +16,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	temp = *head;
 	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 	new->n = n;
 	if (idx == 0)
 	{
@@ -32,5 +34,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	ant->next = new;
 	new->next = temp;
 
-	return (*head);
+	return (new);
 }
