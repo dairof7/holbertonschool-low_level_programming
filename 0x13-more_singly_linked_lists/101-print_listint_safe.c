c553f7024a871a1bfc9668ec232f3aeeaf137bb9
dairof7 <dairof7@gmail.com>
2020-03-24 17:03:50 -0500

    add #101

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_listint_safe - print safe.
+ * @head: head of the list
+ * Return: size of list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	unsigned long len = 0;
+
+	while (head != NULL)
+	{
+		printf("[%p]%d\n", (void *)head, head->n);
+		len++;
+
+		if (head > head->next)
+			head = head->next;
+		else
+			exit(98);
+	}
+
+	return (len);
+}
