e11a03149297e12dda3f86b68ceb3eea6b2d3ce3
dairof7 <dairof7@gmail.com>
2020-03-25 21:15:50 -0500

    add #102

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * free_listint_safe - free safe list.
+ * @h: head of the list
+ * Return: size of list
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	unsigned long len = 0;
+	listint_t *temp = *h;
+
+	if (h == NULL)
+		return (0);
+
+	if (*h == NULL)
+		return (0);
+
+	while (*h != NULL)
+	{
+		*h = (*h)->next;
+		len++;
+		free(temp);
+
+		if (*h > (*h)->next)
+		{
+			*h = NULL;
+			return (len);
+		}
+		temp = *h;
+	}
+
+	return (len);
+}
