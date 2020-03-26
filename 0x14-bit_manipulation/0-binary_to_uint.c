4adc8a5e8d3cf6b15ac390e339f2032d2df7b24f
dairof7 <dairof7@gmail.com>
2020-03-26 13:44:24 -0500

    adding #0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+int _pow_recursion(int x, int y);
+int _strlen(const char *s);
+/**
+ * binary_to_uint - conver a base 2 to base 10
+ * @b: base 2 number
+ * Return: return base 10 number or 0 in error
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0;
+	unsigned int sum = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[i])
+	{
+		if (b[i] == '1')
+		{
+			sum = sum << 1;
+			sum = sum | 1;
+		}
+		else if (b[i] == '0')
+			sum = sum << 1;
+		else
+			return (0);
+		i++;
+		printf("<%d>", sum);
+	}
+
+	return (sum);
+}
---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+#endif
