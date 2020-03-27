b1b60c28e87bfb142338c3c85cda224580ca9b72
dairof7 <dairof7@gmail.com>
2020-03-26 20:23:00 -0500

    add #100

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+/**
+ * get_endianness - checks the endianness.
+ *
+ * Return: 0 if big endian, 1 if little endian.
+ */
+
+int get_endianness(void)
+{
+	int n = 1;
+
+	if (*(char *)&n == 1)
+		return (1);
+	else
+		return (0);
+}
