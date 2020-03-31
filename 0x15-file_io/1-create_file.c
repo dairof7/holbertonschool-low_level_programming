a6b14a2a4c0710328d935e63664f13a8495e2fd8
dairof7 <dairof7@gmail.com>
2020-03-31 17:37:26 -0500

    fix condition

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,11 +20,13 @@ int create_file(const char *filename, char *text_content)
 		i++;
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 	w = write(fd, text_content, i);
 	c = close(fd);
 
-	if ((fd == -1) || (w == -1) || (c == -1))
-		return (0);
+	if ((w == -1) || (c == -1))
+		return (-1);
 
 	return (1);
 
