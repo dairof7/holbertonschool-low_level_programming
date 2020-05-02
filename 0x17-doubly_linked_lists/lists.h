876e2c76917126d5ae29ac273dfe4bc20318c920
dairof7 <dairof7@gmail.com>
2020-05-02 17:11:15 -0500

    betty style

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -7,7 +7,7 @@ typedef struct dlist_s
 {
 	int n;
 	struct dlist_s *next;
-    struct dlist_s *prev;
+	struct dlist_s *prev;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
@@ -19,5 +19,4 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
-
 #endif
