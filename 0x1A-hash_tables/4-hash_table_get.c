dac6c8b8d7dcc25781b379b24cd416476f74e7fd
dairof7 <dairof7@gmail.com>
2020-05-29 09:55:56 -0500

    moving index

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,10 +9,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
 
-	index = key_index((const unsigned char *)key, ht->size);
+	
 	if (ht == NULL || *key == 0 || key == NULL)
 		return (NULL);
-
+	index = key_index((const unsigned char *)key, ht->size);
 	while (ht->array[index])
 	{
 		if (strcmp(ht->array[index]->key, key) == 0)
