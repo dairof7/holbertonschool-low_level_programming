1c5dfc0ce95cf4cb0c5ade9f01a83663f2b16e14
dairof7 <dairof7@gmail.com>
2020-07-03 17:50:41 -0500

    fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,24 +2,23 @@
 
 """ Module to calculate island perimeter """
 
-
 def island_perimeter(grid):
     """ returns the perimeter of the island insert by grid list """
     sum = 0
     x = len(grid)
     y = len(grid[0])
 
-    if x > 0 and y > 0:
+    for i in range(x):
+        for j in range(y):
+            if grid[i][j] == 1:
+                if j - 1 == -1 or grid[i][j - 1] == 0:
+                    sum += 1
+                if j + 1 == y or grid[i][j + 1] == 0:
+                    sum += 1
 
-        for i in range(1, x - 1):
-            for j in range(1, y - 1):
-                if grid[i][j] == 1:
-                    if grid[i][j - 1] == 0:
-                        sum += 1
-                    if grid[i][j + 1] == 0:
-                        sum += 1
-                    if grid[i - 1][j] == 0:
-                        sum += 1
-                    if grid[i + 1][j] == 0:
-                        sum += 1
+                if i - 1 == -1 or grid[i - 1][j] == 0:
+                    sum += 1
+
+                if i + 1 == x or grid[i + 1][j] == 0:
+                    sum += 1
     return sum
