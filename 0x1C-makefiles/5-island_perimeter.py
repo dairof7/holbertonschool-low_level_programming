79bbd3b4ed116c331fb5690213c39fa88443cdf8
dairof7 <dairof7@gmail.com>
2020-07-03 15:32:48 -0500

    adding 35

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+""" Module to calculate island perimeter """
+
+
+def island_perimeter(grid):
+    """ returns the perimeter of the island insert by grid list """
+    sum = 0
+    x = len(grid)
+    y = len(grid[0])
+
+    if (x > 2 and y > 2 and x < 101 and y < 101):
+        for i in range(1, x-1):
+            for j in range(1, y-1):
+                if grid[i][j] == 1:
+                    if grid[i][j - 1] == 0:
+                        sum += 1
+                    if grid[i][j + 1] == 0:
+                        sum += 1
+                    if grid[i - 1][j] == 0:
+                        sum += 1
+                    if grid[i + 1][j] == 0:
+                        sum += 1
+        return sum
