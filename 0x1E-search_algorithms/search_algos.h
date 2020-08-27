c6ec758795bf939e3a6b446fb19d17d2b4bcc81d
dairof7 <dairof7@gmail.com>
2020-08-27 11:54:01 -0500

    add #102

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,4 +11,5 @@ void print_array(int *array, size_t ini, size_t end);
 int jump_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 int advanced_binary_rec(int *array, size_t ini, size_t end, int value);
+int interpolation_search(int *array, size_t size, int value);
 #endif
