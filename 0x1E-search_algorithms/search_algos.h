756e80f63a1b9bbd785356c50766ed2aef56051d
dairof7 <dairof7@gmail.com>
2020-08-27 10:35:31 -0500

    update header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,5 +9,5 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t ini, size_t end);
 int jump_search(int *array, size_t size, int value);
-
+int advanced_binary(int *array, size_t size, int value);
 #endif
