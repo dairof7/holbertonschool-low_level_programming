28095f2d1ec84bea64121c0164414c9a3622777e
dairof7 <dairof7@gmail.com>
2020-08-27 11:56:41 -0500

    add #102

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -14,6 +14,8 @@ int interpolation_search(int *array, size_t size, int value)
 	int high = size - 1;
 	size_t mid;
 
+	mid = low + (((double)(high - low) / (array[high] - array[low])) *
+		(value - array[low]));
 	while ((array[high] != array[low]) && (value >= array[low]) &&
 		(value <= array[high])) {
 		mid = low + (((double)(high - low) / (array[high] - array[low])) *
