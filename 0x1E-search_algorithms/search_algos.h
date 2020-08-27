ea1047793e35216998314ce5ea2320a47753bc6c
dairof7 <dairof7@gmail.com>
2020-08-27 12:31:45 -0500

    add #103

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,4 +12,6 @@ int jump_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 int advanced_binary_rec(int *array, size_t ini, size_t end, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int binary_search2(int *array, int ini, int end, int value);
 #endif
