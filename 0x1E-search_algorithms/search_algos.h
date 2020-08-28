060e0b7ac269506d26abc0e249ae707909a2a1ad
dairof7 <dairof7@gmail.com>
2020-08-27 19:39:25 -0500

    adding files

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,6 +5,23 @@
 #include <stdlib.h>
 #include <math.h>
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t ini, size_t end);
@@ -14,4 +31,5 @@ int advanced_binary_rec(int *array, size_t ini, size_t end, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int binary_search2(int *array, int ini, int end, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 #endif
